2b40e575f550c6d2f38fe972a13a2060490c5293
risto <risto.kowaczewski@gmail.com>
2023-04-22 22:53:03 +0200

    upgrade.

--- src/App.js
+++ src/App.js
@@ -1,39 +1,55 @@
 import React, {useState} from 'react';
 import "regenerator-runtime/runtime";
 import YouTube from "react-youtube";
+import Paper from "@mui/material/Paper";
+import Backdrop from "@mui/material/Backdrop";
+import {HighlightOffRounded} from "@mui/icons-material";
 
 function App({domElement}) {
 
-    const vertical = domElement.getAttribute("data-vertical") || "bottom"
-    const horizontal = domElement.getAttribute("data-horizontal") || "right"
-    const autoHide = domElement.getAttribute("data-auto-hide") || 60000
+    const videoId = domElement.getAttribute("data-video-id") || "9QCqDVAkjqY"
 
     const [open, setOpen] = useState(true)
 
-    const handleClose = (event, reason) => {
-        if (reason === 'clickaway') {
-            return;
-        }
-
-        setOpen(false);
-    };
-
     const opts = {
-        height: '390',
+        height: '360',
         width: '640',
         playerVars: {
             // https://developers.google.com/youtube/player_parameters
             autoplay: 1,
+            controls: 0,
+            fs: 0,
+            modestbranding: 1
 
         },
     };
 
-    const handleReady = (event) => {
-        console.log(event)
-        event.target.playVideo()
-    }
+    return <Backdrop
+        open={open}
+    >
+    <div style={{display: "grid"}}>
+        <Paper elevation={8} style={{
+            alignItems: "center",
+            justifyContent: "center",
+            padding: 20,
+            borderRadius: 14}}
+        >
+            <div style={{
+                display: "flex",
+                justifyContent: "end",
+                color: "gray",
+                width: "100%",
+                marginBottom: 10,
+                cursor: "pointer"
+            }}
+                 onClick={() => setOpen(false)}>
+                <HighlightOffRounded/>
+            </div>
+            <YouTube videoId={videoId} opts={opts} />
+        </Paper>
 
-    return <YouTube videoId="2g811Eo7K8U" opts={opts} />
+    </div>
+    </Backdrop>
 }
 
 export default App;
